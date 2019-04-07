ab2813a54cf24640822a0c410c97642c9f68112f
VladVons <vladvons@gmail.com>
2019-04-07 03:44:51 +0300

    MyCommit

--- src/Api/Misc.py
+++ src/Api/Misc.py
@@ -18,8 +18,8 @@ def Version():
 
     Result = {
         "Now":       time.strftime('%Y-%m-%d %H:%M:%S'),
-        "vDate":     '2019.04.05',
-        "vSoftware": '1.0.31',
+        "vDate":     '2019.04.07',
+        "vSoftware": '1.0.32',
         "Hardware":  '1.0.2',
         "Author":    'Volodymyr Vons, VladVons@gmail.com, UA',
         "Homepage":  'http://oster.com.ua/software',
