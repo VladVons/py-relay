05a5a720139319e8e9929fae6916c973a8772604
VladVons <vladvons@gmail.com>
2019-05-06 16:57:46 +0300

    MyCommit

--- install.sh
+++ install.sh
@@ -8,16 +8,22 @@
 
 Nuitka()
 {
-  CODENAME=`grep UBUNTU_CODENAME /etc/os-release | cut -d= -f2`
-  if ["$CODENAME"] = ""]
-  then
-    CODENAME=`lsb_release -c -s`
-  fi;
+  #http://nuitka.net/pages/download.html
 
-  wget -O - http://nuitka.net/deb/archive.key.gpg | apt-key add -
-  echo > /etc/apt/sources.list.d/nuitka.list "deb http://nuitka.net/deb/stable/$CODENAME $CODENAME main"
-  apt-get update
-  apt-get install nuitka
+  #CODENAME=`lsb_release -c -s`
+  #wget -O - http://nuitka.net/deb/archive.key.gpg | apt-key add -
+  #echo > /etc/apt/sources.list.d/nuitka.list "deb http://nuitka.net/deb/stable/$CODENAME $CODENAME main"
+  #apt-get update
+  #apt-get install nuitka --no-install-recommends
+
+  # we need nuitka for python 2.7
+  pip install nuitka
+echo "\
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import nuitka.__main__
+nuitka.__main__.main()" > /usr/bin/nuitka
 }
 
 
